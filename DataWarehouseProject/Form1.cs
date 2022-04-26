fd23f93c4d4540f8eed66f7aa4a35cf876c7542f
felicia graciella <fgraciella@student.ciputra.ac.id>
2022-04-26 15:13:51 +0700

    Add project files.

---
+++ DataWarehouseProject/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DataWarehouseProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection sqlConnect;
+        MySqlCommand sqlCommand;
+        MySqlDataAdapter sqlAdapter;
+        string sqlquery = "";
+        string connectstring = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DW_ZEFANYA_OLAP;";
+    }
+}
