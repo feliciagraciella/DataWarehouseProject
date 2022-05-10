077bbd7e2a119f966ff3faa1baea8476f10dfc6c
felicia graciella <fgraciella@student.ciputra.ac.id>
2022-05-10 15:01:37 +0700

    .

--- DataWarehouseProject/Form1.Designer.cs
+++ DataWarehouseProject/Form1.Designer.cs
@@ -29,60 +29,153 @@ namespace DataWarehouseProject
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbView = new System.Windows.Forms.ComboBox();
+            this.dgvView = new System.Windows.Forms.DataGridView();
+            this.tbCabang = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbReseller = new System.Windows.Forms.TextBox();
+            this.tbProduk = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvView)).BeginInit();
             this.SuspendLayout();
+            // 
+            // cbView
+            // 
+            this.cbView.FormattingEnabled = true;
+            this.cbView.Items.AddRange(new object[] {
+            "ALL",
+            "CABANG",
+            "RESELLER",
+            "PRODUK",
+            "CABANG + PRODUK",
+            "CABANG + RESELLER",
+            "RESELLER + PRODUK"});
+            this.cbView.Location = new System.Drawing.Point(125, 61);
+            this.cbView.Name = "cbView";
+            this.cbView.Size = new System.Drawing.Size(189, 24);
+            this.cbView.TabIndex = 1;
+            this.cbView.SelectedIndexChanged += new System.EventHandler(this.cbView_SelectedIndexChanged);
+            // 
+            // dgvView
+            // 
+            this.dgvView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvView.Location = new System.Drawing.Point(67, 134);
+            this.dgvView.Name = "dgvView";
+            this.dgvView.RowHeadersWidth = 51;
+            this.dgvView.RowTemplate.Height = 24;
+            this.dgvView.Size = new System.Drawing.Size(1142, 493);
+            this.dgvView.TabIndex = 2;
+            this.dgvView.RowStateChanged += new System.Windows.Forms.DataGridViewRowStateChangedEventHandler(this.dgvView_RowStateChanged);
+            // 
+            // tbCabang
+            // 
+            this.tbCabang.Location = new System.Drawing.Point(698, 63);
+            this.tbCabang.Name = "tbCabang";
+            this.tbCabang.Size = new System.Drawing.Size(141, 22);
+            this.tbCabang.TabIndex = 3;
+            this.tbCabang.TextChanged += new System.EventHandler(this.tbCabang_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(73, 61);
+            this.label1.Location = new System.Drawing.Point(64, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.Size = new System.Drawing.Size(37, 17);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "View";
             // 
-            // comboBox1
+            // label2
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(146, 61);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(189, 24);
-            this.comboBox1.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(695, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Search by Cabang";
             // 
-            // dataGridView1
+            // label3
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(67, 160);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(666, 271);
-            this.dataGridView1.TabIndex = 2;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(882, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Search by Reseller";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1065, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Search by Produk";
+            // 
+            // tbReseller
+            // 
+            this.tbReseller.Location = new System.Drawing.Point(885, 63);
+            this.tbReseller.Name = "tbReseller";
+            this.tbReseller.Size = new System.Drawing.Size(141, 22);
+            this.tbReseller.TabIndex = 9;
+            this.tbReseller.TextChanged += new System.EventHandler(this.tbReseller_TextChanged);
+            // 
+            // tbProduk
+            // 
+            this.tbProduk.Location = new System.Drawing.Point(1068, 64);
+            this.tbProduk.Name = "tbProduk";
+            this.tbProduk.Size = new System.Drawing.Size(141, 22);
+            this.tbProduk.TabIndex = 10;
+            this.tbProduk.TextChanged += new System.EventHandler(this.tbProduk_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(64, 669);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "0 Rows";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(940, 522);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(1303, 732);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbProduk);
+            this.Controls.Add(this.tbReseller);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCabang);
+            this.Controls.Add(this.dgvView);
+            this.Controls.Add(this.cbView);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Text = "Jam Tangan";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
+        private System.Windows.Forms.ComboBox cbView;
+        private System.Windows.Forms.DataGridView dgvView;
+        private System.Windows.Forms.TextBox tbCabang;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbReseller;
+        private System.Windows.Forms.TextBox tbProduk;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- DataWarehouseProject/Form1.cs
+++ DataWarehouseProject/Form1.cs
@@ -17,10 +17,338 @@ namespace DataWarehouseProject
         {
             InitializeComponent();
         }
-        MySqlConnection sqlConnect;
+        MySqlConnection sqlConnection;
         MySqlCommand sqlCommand;
         MySqlDataAdapter sqlAdapter;
-        string sqlquery = "";
-        string connectstring = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DW_ZEFANYA_OLAP;";
+        string sqlQuery = "";
+        //string connectstring = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DW_ZEFANYA_OLAP;";
+        string cabang;
+        string reseller;
+        string produk;
+
+        private void openConnection()
+        {
+            sqlConnection = new MySqlConnection();
+            sqlConnection.ConnectionString = "server=139.255.11.84;uid=student;pwd=isbmantap;database=DW_ZEFANYA_OLAP;";
+
+            try
+            {
+                if (sqlConnection.State == ConnectionState.Closed)
+                {
+                    sqlConnection.Open();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void cbView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                openConnection();
+                tbCabang.Text = "";
+                tbProduk.Text = "";
+                tbReseller.Text = "";
+
+                if (cbView.SelectedItem == "ALL")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "SELECT NAMA_CABANG as `Nama Cabang`, ID_SALES as `ID Sales`,  R_ID as `ID Reseller`, R_NAMA as `Nama Reseller`, J_SKU as `SKU`, PRODUK as `Nama Produk`, HARGA as `Harga`, QTY as `Quantity` FROM `VIEW_SALES`";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbCabang.Enabled = true;
+                    tbProduk.Enabled = true;
+                    tbReseller.Enabled = true;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "CABANG")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "SELECT nama_cabang as `Nama Cabang`, concat(sum(qty), ' pcs') as `Total Penjualan`, concat('Rp ', format(sum(harga), 0)) as `Total Pendapatan` FROM DW_ZEFANYA_OLAP.VIEW_SALES group by id_cabang";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbCabang.Enabled = true;
+                    tbProduk.Enabled = false;
+                    tbReseller.Enabled = false;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "RESELLER")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "select r_id as `ID Reseller`, r_nama as `Nama Reseller`, concat(sum(qty), ' pcs') as `Total Pembelian`, concat('Rp ', format(sum(harga), 0)) as `Total Harga (Rp)` FROM DW_ZEFANYA_OLAP.VIEW_SALES group by r_id order by r_id asc";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbReseller.Enabled = true;
+                    tbProduk.Enabled = false;
+                    tbCabang.Enabled = false;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "PRODUK")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "select j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga` FROM DW_ZEFANYA_OLAP.VIEW_SALES group by j_sku, produk order by j_sku asc";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbProduk.Enabled = true;
+                    tbCabang.Enabled = false;
+                    tbReseller.Enabled = false;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "CABANG + RESELLER")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "select nama_cabang as `Nama Cabang`, r_id as `ID Reseller`, r_nama as `Nama Reseller`, concat(sum(qty), ' pcs') as `Total Pembelian`, concat('Rp ', format(sum(harga), 0)) as `Total Harga(Rp)` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%"+cabang+"%' and r_nama like '%"+reseller+"%' group by r_id, id_cabang order by r_id asc";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbCabang.Enabled = true;
+                    tbReseller.Enabled = true;
+                    tbProduk.Enabled = false;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "CABANG + PRODUK")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "select nama_cabang as `Nama Cabang`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' group by j_sku, produk, nama_cabang having `Nama Produk` like '%"+produk+"%'order by nama_cabang asc";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbCabang.Enabled = true;
+                    tbReseller.Enabled = false;
+                    tbProduk.Enabled = true;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+                else if (cbView.SelectedItem == "RESELLER + PRODUK")
+                {
+                    DataTable dtView = new DataTable();
+                    sqlQuery = "select r_id as `ID Reseller`, r_nama as `Nama Reseller`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga`FROM DW_ZEFANYA_OLAP.VIEW_SALES where r_nama like '%" + reseller + "%'group by j_sku, produk, r_id having `Nama Produk` like '%" + produk + "%'order by r_id asc";
+                    MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                    sqlAdapter.Fill(dtView);
+
+                    dgvView.DataSource = dtView;
+
+                    tbCabang.Enabled = false;
+                    tbProduk.Enabled = true;
+                    tbReseller.Enabled = true;
+                    label5.Text = dtView.Rows.Count + " Rows";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void tbCabang_TextChanged(object sender, EventArgs e)
+        {
+            cabang = tbCabang.Text.ToString();
+            if (cbView.SelectedItem == "ALL")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "SELECT NAMA_CABANG as `Nama Cabang`, ID_SALES as `ID Sales`,  R_ID as `ID Reseller`, R_NAMA as `Nama Reseller`, J_SKU as `SKU`, PRODUK as `Nama Produk`, HARGA as `Harga`, QTY as `Quantity` FROM `VIEW_SALES` where nama_cabang like '%" + cabang + "%' and r_nama like '%" + reseller + "%' having `Nama Produk` like '%" + produk + "%'";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbProduk.Enabled = true;
+                tbReseller.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "CABANG")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "SELECT nama_cabang as `Nama Cabang`, concat(sum(qty), ' pcs') as `Total Penjualan`, concat('Rp ', format(sum(harga), 0)) as `Total Pendapatan` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' group by id_cabang ";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbProduk.Enabled = false;
+                tbReseller.Enabled = false;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "CABANG + PRODUK")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select nama_cabang as `Nama Cabang`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' group by j_sku, produk, nama_cabang having `Nama Produk` like '%" + produk + "%' order by nama_cabang asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbReseller.Enabled = false;
+                tbProduk.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "CABANG + RESELLER")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select nama_cabang as `Nama Cabang`, r_id as `ID Reseller`, r_nama as `Nama Reseller`, concat(sum(qty), ' pcs') as `Total Pembelian`, concat('Rp ', format(sum(harga), 0)) as `Total Harga(Rp)` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' and r_nama like '%" + reseller + "%' group by r_id, id_cabang order by r_id asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbReseller.Enabled = true;
+                tbProduk.Enabled = false;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+        }
+
+        private void tbReseller_TextChanged(object sender, EventArgs e)
+        {
+            reseller = tbReseller.Text.ToString();
+            if (cbView.SelectedItem == "ALL")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "SELECT NAMA_CABANG as `Nama Cabang`, ID_SALES as `ID Sales`,  R_ID as `ID Reseller`, R_NAMA as `Nama Reseller`, J_SKU as `SKU`, PRODUK as `Nama Produk`, HARGA as `Harga`, QTY as `Quantity` FROM `VIEW_SALES` where nama_cabang like '%" + cabang + "%' and r_nama like '%" + reseller + "%' having `Nama Produk` like '%" + produk + "%'";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbProduk.Enabled = true;
+                tbReseller.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "RESELLER")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select r_id as `ID Reseller`, r_nama as `Nama Reseller`, concat(sum(qty), ' pcs') as `Total Pembelian`, concat('Rp ', format(sum(harga), 0)) as `Total Harga (Rp)` FROM DW_ZEFANYA_OLAP.VIEW_SALES where r_nama like '%" + reseller + "%' group by r_id order by r_id asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbReseller.Enabled = true;
+                tbProduk.Enabled = false;
+                tbCabang.Enabled = false;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "CABANG + RESELLER")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select nama_cabang as `Nama Cabang`, r_id as `ID Reseller`, r_nama as `Nama Reseller`, concat(sum(qty), ' pcs') as `Total Pembelian`, concat('Rp ', format(sum(harga), 0)) as `Total Harga(Rp)` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' and r_nama like '%" + reseller + "%' group by r_id, id_cabang order by r_id asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbReseller.Enabled = true;
+                tbProduk.Enabled = false;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "RESELLER + PRODUK")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select r_id as `ID Reseller`, r_nama as `Nama Reseller`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga`FROM DW_ZEFANYA_OLAP.VIEW_SALES where r_nama like '%" + reseller + "%' group by j_sku, produk, r_id having `Nama Produk` like '%" + produk + "%'order by r_id asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = false;
+                tbProduk.Enabled = true;
+                tbReseller.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+        }
+
+        private void tbProduk_TextChanged(object sender, EventArgs e)
+        {
+            produk = tbProduk.Text.ToString();
+            if (cbView.SelectedItem == "ALL")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "SELECT NAMA_CABANG as `Nama Cabang`, ID_SALES as `ID Sales`,  R_ID as `ID Reseller`, R_NAMA as `Nama Reseller`, J_SKU as `SKU`, PRODUK as `Nama Produk`, HARGA as `Harga`, QTY as `Quantity` FROM `VIEW_SALES` where nama_cabang like '%" + cabang + "%' and r_nama like '%" + reseller + "%' having `Nama Produk` like '%" + produk + "%'";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbProduk.Enabled = true;
+                tbReseller.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "PRODUK")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga` FROM DW_ZEFANYA_OLAP.VIEW_SALES group by j_sku, produk having `Nama Produk` like '%" + produk + "%' order by j_sku asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+                dgvView.Refresh();
+                dgvView.DataSource = dtView;
+                
+                tbProduk.Enabled = true;
+                tbCabang.Enabled = false;
+                tbReseller.Enabled = false;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "CABANG + PRODUK")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select nama_cabang as `Nama Cabang`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga` FROM DW_ZEFANYA_OLAP.VIEW_SALES where nama_cabang like '%" + cabang + "%' group by j_sku, produk, nama_cabang having `Nama Produk` like '%" + produk + "%' order by nama_cabang asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = true;
+                tbReseller.Enabled = false;
+                tbProduk.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+            else if (cbView.SelectedItem == "RESELLER + PRODUK")
+            {
+                DataTable dtView = new DataTable();
+                sqlQuery = "select r_id as `ID Reseller`, r_nama as `Nama Reseller`, j_sku as `SKU`, produk as `Nama Produk`, concat(sum(qty), ' pcs') as `Quantity`, concat('Rp ', format(sum(harga), 0)) as `Total Harga`FROM DW_ZEFANYA_OLAP.VIEW_SALES where r_nama like '%" + reseller + "%'group by j_sku, produk, r_id having `Nama Produk` like '%" + produk + "%'order by r_id asc";
+                MySqlDataAdapter sqlAdapter = new MySqlDataAdapter(sqlQuery, sqlConnection);
+                sqlAdapter.Fill(dtView);
+
+                dgvView.DataSource = dtView;
+
+                tbCabang.Enabled = false;
+                tbProduk.Enabled = true;
+                tbReseller.Enabled = true;
+                label5.Text = dtView.Rows.Count + " Rows";
+            }
+        }
+
+        private void dgvView_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbView.SelectedIndex = 0;
+        }
     }
 }
